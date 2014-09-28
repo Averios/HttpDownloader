8a05cb393d750a27a2da87abeef130443a880d50
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2014-09-29 00:39:15 +0700

    Add converter from domain to sockaddr_in

--- Downloader.c
+++ Downloader.c
@@ -1,17 +1,21 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <netdb.h>
 
-int hostname_to_ip(char*, char*);
+int hostname_to_ip(char* domain, struct sockaddr_in *_addr);
+int get_domain(char *address, char *domain, char *destionation);
 
 int main(int argc, char** argv){
     char buffer[1025];
     int portNum, sockfd = 0, ok;
+    struct sockaddr_in serv_addr;
+    
     if(argc < 2){
         portNum = atoi(argv[2]);
     }
@@ -42,12 +46,49 @@ int main(int argc, char** argv){
             destination[k] = argv[1][j];
         }
     }
-    printf("%s\n",domain);
-    printf("%s\n",destination);
-
+    
+    hostname_to_ip(domain, &serv_addr);
+    
+    
+    
+    
     return 0;
 }
 
-int hostname_to_ip(char *domain, char* ip){
+//http://www.binarytides.com/hostname-to-ip-address-c-sockets-linux/
+int hostname_to_ip(char *domain, struct sockaddr_in *_addr){
+    
+    struct addrinfo hint, *serv_addr, *iter;
+    struct sockaddr_in *h;
+    int rv;
+    
+    memset(&hint, 0, sizeof hint);
+    
+    hint.ai_family = AF_INET;       //Force ipv4
+    hint.ai_socktype = SOCK_STREAM; //TCP
+    
+    if((rv = getaddrinfo(domain, "http", &hint, &serv_addr)) != 0){
+        //Print the error
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+    
+    //Dummy variable for testing purpose
+    char *ip;
+    ip = (char*)malloc(sizeof(char)*100);
+    
+    //Loop through all the result
+    for(iter = serv_addr; iter != NULL; iter->ai_next){
+        h = (struct sockaddr_in*)iter->ai_addr;
+        if(strcpy(ip,inet_ntoa(h->sin_addr)) != NULL){
+            //Stop the loop when the connection is successful
+            *_addr = *h;
+            break;
+        }
+    }
+    
+    freeaddrinfo(serv_addr);
+    free(ip);
+    return 0;
     
 }
