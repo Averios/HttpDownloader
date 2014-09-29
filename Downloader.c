70b9ec784909468efd680ee9114be07128076208
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2014-09-29 18:09:19 +0700

    Changing the get_domain function to use strtok_r. Some error found when testing with lagre url

--- Downloader.c
+++ Downloader.c
@@ -9,13 +9,13 @@
 #include <netdb.h>
 
 int hostname_to_ip(char* domain, struct sockaddr_in *_addr);
-void get_domain(char *address, char **domain, char **destination);
+void get_domain(char *address, char **domain, char **destination, char **file);
 
 int main(int argc, char** argv){
     char buffer[1025];
     int portNum, sockfd = 0;
     struct sockaddr_in serv_addr;
-    char *domain,*destination;
+    char *domain,*destination,*file;
     
     if(argc < 2){
         portNum = atoi(argv[2]);
@@ -24,8 +24,9 @@ int main(int argc, char** argv){
         portNum = 80;
     }
 
-    get_domain(argv[1], &domain, &destination);
+    get_domain(argv[1], &domain, &destination, &file);
     
+    printf("%s\n%s\n%s\n", domain, destination, file);
     hostname_to_ip(domain, &serv_addr);
     
     printf("%s resolved %s\n", domain, inet_ntoa(serv_addr.sin_addr));
@@ -62,6 +63,7 @@ int hostname_to_ip(char *domain, struct sockaddr_in *_addr){
     hint.ai_family = AF_INET;       //Force ipv4
     hint.ai_socktype = SOCK_STREAM; //TCP
     
+    //resolving domain
     if((rv = getaddrinfo(domain, "http", &hint, &serv_addr)) != 0){
         //Print the error
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -89,33 +91,27 @@ int hostname_to_ip(char *domain, struct sockaddr_in *_addr){
 }
 
 //Separate the domain and file from the url
-void get_domain(char *address, char **domain, char **destination){
-    int length;
-    length = strlen(address);
-    char *dom, *dest;
-    int i,separator;
-    for(i = 0; i < length; i++){
-        if(address[i] == '/'){
-            separator = i;
-            break;
-        }
-    }
-    dom = (char*)malloc(sizeof(char)*(i+1));
-    if(i == length){
-        strcpy(dom,address);
+void get_domain(char *address, char **domain, char **destination, char **file){
+    char *dom, *dest, *fl;
+    char *temp;
+    dom = strtok_r(address, "/", &dest);
+    if(strcmp(dest,"") == 0){
         dest = (char*)malloc(sizeof(char)*2);
-        strcpy(dest,"/");
+        strcpy(dest, "/");
     }
-    else{
-        strncpy(dom,address,i);
-        dest = (char*)malloc(sizeof(char)*(length-i+1));
-        int j = i,k;
-        for(k = 0; j < length; j++, k++){
-            dest[k] = address[j];
-        }
-        
+    fl = (char*)malloc(sizeof(dest));
+    temp = dest;
+    while(strcmp(temp, "") != 0){
+        strcpy(fl, temp);
+        fl = strtok_r(fl, "/", &temp);
     }
-
+    if(fl == NULL){
+        fl = (char*)malloc(sizeof(char)*4);
+        strcpy(fl, "any");
+    }
+    
+    
     *domain = dom;
     *destination = dest;
+    *file = fl;
 }
